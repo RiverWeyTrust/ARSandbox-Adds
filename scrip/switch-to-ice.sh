dbb3af145f136b865c56b1af4f4183f8af645025
AJY <65818713+RiverWeyTrust@users.noreply.github.com>
2020-07-15 09:59:18 +0100

    Update switch-to-ice.sh

--- scrip/switch-to-ice.sh
+++ scrip/switch-to-ice.sh
@@ -2,4 +2,4 @@ cp ~/scrip/SurfaceAddWaterColor-SparklyIce.fs ~/src/SARndbox-2.7/share/SARndbox-
 ## Attenuation at 0.5
 echo "waterAttenuation 0.5" > SARndbox.fifo
 ## ice speed changes pipe for SNOW to 0.1
-echo "waterSpeed 0.1" > SARndbox.fifox 
+echo "waterSpeed 0.1" > SARndbox.fifo 
