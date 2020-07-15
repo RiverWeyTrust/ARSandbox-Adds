e2d60c41373a527a845a65bd2380c21442c51801
AJY <65818713+RiverWeyTrust@users.noreply.github.com>
2020-07-15 09:58:44 +0100

    Update switch-to-toxic.sh

--- scrip/switch-to-toxic.sh
+++ scrip/switch-to-toxic.sh
@@ -2,4 +2,4 @@ cp ~/scrip/SurfaceAddWaterColor-ToxicWaste.fs ~/src/SARndbox-2.7/share/SARndbox-
 ## Attenuation at 0.12
 echo "waterAttenuation 0.12" > SARndbox.fifo
 ## water speed changes pipe for Toxic to 0.06
-echo "waterSpeed 0.06" > SARndbox.fifox 
+echo "waterSpeed 0.06" > SARndbox.fifo 
