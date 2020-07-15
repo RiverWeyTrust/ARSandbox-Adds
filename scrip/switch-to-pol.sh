5dee1fa1386315266c1d4274def7a9821e1f5a15
AJY <65818713+RiverWeyTrust@users.noreply.github.com>
2020-07-15 10:00:29 +0100

    Update switch-to-pol.sh

--- scrip/switch-to-pol.sh
+++ scrip/switch-to-pol.sh
@@ -2,4 +2,4 @@ cp ~/scrip/SurfaceAddWaterColor-PollutedWater.fs ~/src/SARndbox-2.7/share/SARndb
 ## Attenuation at 0.3
 echo "waterAttenuation 0.3" > SARndbox.fifo
 ## ice speed changes pipe for pollution to 0.9
-echo "waterSpeed 0.9" > SARndbox.fifox 
+echo "waterSpeed 0.9" > SARndbox.fifo 
