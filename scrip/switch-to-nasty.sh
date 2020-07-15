f861197b21919dfad54e0b0ba65eb06bcb70cbee
AJY <65818713+RiverWeyTrust@users.noreply.github.com>
2020-07-15 09:59:58 +0100

    Update switch-to-nasty.sh

--- scrip/switch-to-nasty.sh
+++ scrip/switch-to-nasty.sh
@@ -1,4 +1,4 @@
 cp ~/scrip/SurfaceAddWaterColor-ToxicDeath.fs ~/src/SARndbox-2.7/share/SARndbox-2.7/Shaders/SurfaceAddWaterColor.fs
 echo "waterAttenuation 0.8" > SARndbox.fifo
 ## water speed changes pipe for Nasty  to 0.06
-echo "waterSpeed 0.03" > SARndbox.fifox 
+echo "waterSpeed 0.03" > SARndbox.fifo 
